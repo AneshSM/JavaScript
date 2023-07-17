5a801586ed9b2dcbd7a10c84023e30d12901041a
AneshSM <64136492+AneshSM@users.noreply.github.com>
2023-07-17 17:50:12 +0530

    changes

---
+++ Test/server/test-port.js
@@ -0,0 +1,19 @@
+var port = normalizePort(process.env.PORT || "4004");
+console.log(typeof port);
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
+module.exports = port;
